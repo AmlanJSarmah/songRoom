fbf8b14c73581192bf056f79d4b7013e76b8693f
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2025-08-23 11:29:07 +0530

    Added chat management

--- songRoomUi/src/components/Chat.jsx
+++ songRoomUi/src/components/Chat.jsx
@@ -1,22 +1,45 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+
 export function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [chat, setChat] = useState("");
+  const { user } = useAuth();
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setChat(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setMessages([...messages, chat]);
+    setChat("");
   };
 
   return (
     <div className="chat">
       {/* <!-- Message form --> */}
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Type your message here..." />
+        <input
+          type="text"
+          placeholder="Type your message here..."
+          onChange={handleChange}
+          value={chat}
+        />
         <button type="submit">📩</button>
       </form>
       {/* <!-- Chat --> */}
       <section className="chat-window">
         {/* <!-- Individual message go here --> */}
-        <div className="message">
-          <h3>Daniel</h3>
-          <h2>Hello there!</h2>
-        </div>
+        {messages.map((message, index) => {
+          return (
+            <div className="message" key={index}>
+              <h3>{user.username}</h3>
+              <h2>{message}</h2>
+            </div>
+          );
+        })}
         {/* <!-- Chat end here --> */}
       </section>
     </div>
